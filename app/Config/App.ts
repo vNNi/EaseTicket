4a11a5dbedba7523167e8f580cf1172358406a9c
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-03-30 23:18:01 -0300

    adding graphql basics

--- app/Config/App.ts
+++ app/Config/App.ts
@@ -1,16 +1,51 @@
 import express from 'express';
 import cors from 'cors';
+import graphqlHttp from 'express-graphql';
+import { buildSchema } from 'graphql';
 
 class App {
     public express: express.Application;
 
     public constructor() {
         this.express = express();
+        this.middlewares();
     }
     private middlewares(): void {
         this.express.use(express.json());
         this.express.use(cors());
         this.express.use(express.urlencoded({ extended: true }));
+        this.express.use(
+            '/graphql',
+            graphqlHttp({
+                schema: buildSchema(`
+                type Event {
+                    _id: ID!,
+                    title: String!,
+                    description: String!,
+                    price: Float!,
+                    date: String!
+                }
+                input EventInput {
+                    title: String!,
+                    description: String!,
+                    price: Float!,
+                    date: String!
+                }
+                type RootQuery {
+                    events: [Event!]!,
+                    name: String!
+                }
+                type RootMutation {
+                    createEvent(eventInput: EventInput): Event
+                }
+                schema {
+                    query: RootQuery
+                    mutation: RootMutation
+                }`),
+                rootValue: {},
+                graphiql: true,
+            }),
+        );
     }
 }
 export default new App().express;
