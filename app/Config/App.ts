5e51cea81d8e2ac02e4810833c8c42bebfdf1863
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-03-31 15:51:27 -0300

    add graphql and create verifyToken query

--- app/Config/App.ts
+++ app/Config/App.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import graphqlHttp from 'express-graphql';
-import { buildSchema } from 'graphql';
-
+import Schemas from '../GraphQl/Schemas/index';
+import Resolvers from '../GraphQl/Resolvers/index';
 class App {
     public express: express.Application;
 
@@ -17,32 +17,8 @@ class App {
         this.express.use(
             '/graphql',
             graphqlHttp({
-                schema: buildSchema(`
-                type Event {
-                    _id: ID!,
-                    title: String!,
-                    description: String!,
-                    price: Float!,
-                    date: String!
-                }
-                input EventInput {
-                    title: String!,
-                    description: String!,
-                    price: Float!,
-                    date: String!
-                }
-                type RootQuery {
-                    events: [Event!]!,
-                    name: String!
-                }
-                type RootMutation {
-                    createEvent(eventInput: EventInput): Event
-                }
-                schema {
-                    query: RootQuery
-                    mutation: RootMutation
-                }`),
-                rootValue: {},
+                schema: Schemas,
+                rootValue: Resolvers,
                 graphiql: true,
             }),
         );
--- app/index.ts
+++ app/index.ts
@@ -3,7 +3,7 @@ import App from './Config/App';
 import customEnv from 'custom-env';
 customEnv.env(true);
 
-const url = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds119085.mlab.com:19085/maapp`;
+const url = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds149329.mlab.com:49329/easeticket`;
 const db = new Database(url);
 
 db.connect()
