b69ecc89b2bd5b877a3b67d9eed290ef22c2233a
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-04-08 11:11:51 -0300

    adjust in types of graphql schemas

--- app/GraphQl/Schemas/index.ts
+++ app/GraphQl/Schemas/index.ts
@@ -1,41 +1,50 @@
 import { buildSchema } from 'graphql';
-const schema = buildSchema(`
+const tokeType = `
 type Token {
     token: String!
 }
+`;
+const schema = buildSchema(`
+${tokeType}
 type Payload {
     email: String!
     userId: String!
 }
-type Movie: {
+type Movie {
         title: String!
         description: String
 }
 type Session {
     _id: ID!
     movie: Movie!
-    price: Number
+    price: Float!
     openAt: String
     closeAt: String
-    sessionLimit: Number,
-    buyers: [String]!,
+    sessionLimit: Int
+    buyers: [String]!
+}
+input movieInput{
+    title: String!
+    description: String
 }
 input sessionInput {
-    movie: Movie!,
-    price: Number!,
-    openAt: String!,
-    closeAt: String!,
-    sessionLimit: Number!,
-    buyers: [String]!,
+    movie: movieInput!
+    price: Float!
+    openAt: String!
+    closeAt: String!
+    sessionLimit: Int!
 }
 input tokenInput {
     token: String!
 }
 type RootQuery {
-    verifyToken(tokenInput: tokenInput): Payload,
-    createSession(sessionInput: sessionInput): Session,
+    verifyToken(tokenInput: tokenInput): Payload
+}
+type RootMutation {
+    createSession(sessionInput: sessionInput): Session
 }
 schema {
     query: RootQuery
+    mutation: RootMutation
 }`);
 export default schema;
