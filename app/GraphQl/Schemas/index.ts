27c52a5db0037b52d47fdf6036cd273ddc783354
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-04-07 23:24:13 -0300

    add resolver to verify if users is on sessions and query to create a session

--- app/GraphQl/Schemas/index.ts
+++ app/GraphQl/Schemas/index.ts
@@ -7,11 +7,33 @@ type Payload {
     email: String!
     userId: String!
 }
+type Movie: {
+        title: String!
+        description: String
+}
+type Session {
+    _id: ID!
+    movie: Movie!
+    price: Number
+    openAt: String
+    closeAt: String
+    sessionLimit: Number,
+    buyers: [String]!,
+}
+input sessionInput {
+    movie: Movie!,
+    price: Number!,
+    openAt: String!,
+    closeAt: String!,
+    sessionLimit: Number!,
+    buyers: [String]!,
+}
 input tokenInput {
     token: String!
 }
 type RootQuery {
     verifyToken(tokenInput: tokenInput): Payload,
+    createSession(sessionInput: sessionInput): Session,
 }
 schema {
     query: RootQuery
