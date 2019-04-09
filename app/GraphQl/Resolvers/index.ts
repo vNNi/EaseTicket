f91e3822a416f5a7847f067df4da633c8289a414
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-04-08 22:39:28 -0300

    add new resolver to index

--- app/GraphQl/Resolvers/index.ts
+++ app/GraphQl/Resolvers/index.ts
@@ -1,6 +1,8 @@
 import authResolver from '../Resolvers/Auth';
 import sessionResolver from '../Resolvers/Session';
+import userResolver from '../Resolvers/User';
 const rootResolver = {
+    ...userResolver,
     ...sessionResolver,
     ...authResolver,
 };
