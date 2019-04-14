dd693c30e80921b5e7f3fbff28d292bad1c05fc9
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-04-14 20:47:49 -0300

    new resolver to get user and one user with id

--- app/GraphQl/Resolvers/User.ts
+++ app/GraphQl/Resolvers/User.ts
@@ -1,8 +1,7 @@
 import User from '../../Models/Users';
 
 const user = {
-    createUser: args => {
-        console.log(args);
+    createUser: (args: any) => {
         const user = new User({
             name: args.userInput.name,
             cpf: args.userInput.cpf,
@@ -16,12 +15,31 @@ const user = {
             return user
                 .save()
                 .then(result2 => {
-                    console.log(result2);
                     return result2;
                 })
                 .catch(err => {
                     throw new Error(err);
                 });
+        });
+    },
+    getUser: (args: any) => {
+        var id = args.userId || undefined;
+        if (id) {
+            return User.findById(id).then(user => {
+                if (user) {
+                    return user;
+                } else {
+                    throw new Error('User undefined');
+                }
+            });
+        }
+    },
+    getAllUsers: (args: any) => {
+        return User.find().then(users => {
+            console.log(users);
+            return {
+                users: users,
+            };
         });
     },
 };
